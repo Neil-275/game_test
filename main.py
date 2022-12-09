9e2384d3141e3656533f0e6c302fdda97950a7b5
Neil-275 <onghoangcodebug01@gmail.com>
2022-12-09 22:43:37 +0700

    :V

--- main.py
+++ main.py
@@ -15,19 +15,20 @@ icon= pygame.image.load('logo.png')
 pygame.display.set_icon(icon)
 
 #Laser
-laserImg=pygame.image.load("laser.png")
+laserImg=pygame.image.load("medical.png")
 laserState=0
 laserX,laserY=0,0
-diLaserX, diLaserY=0,0
+laserA,laserB =0,0
+diLaser=0
 
 #Player
-playerImg=pygame.image.load("cloud.png")
+playerImg=pygame.image.load("worldwide.png")
 playerX,playerY= 268,250
 changeX,changeY=0,0
 
 #enemies
 enemiesImg=pygame.image.load("ufo.png")
-eX,eY,diX,diY,speedY=-129,0,1,1,0
+eX,eY,diX,diY,speedY=0,0,1,1,0
 
 
 def player(x,y):
@@ -40,29 +41,27 @@ def laser(x,y):
     screen.blit(laserImg,(x,y))
 
 def fire():
-    global playerX,playerY,eX,eY,diLaserX,diLaserY,laserX,laserY,laserState
+    global playerX,playerY,eX,eY,laserA,laserB,laserX,laserY,laserState,diLaser
     laserState=1
-    a=(playerY-eY)/(playerX-eX)
+    diLaser= abs(playerX-eX)/(playerX-eX)
+    a=(playerY-eY)/(playerX-eX) 
     b=playerY- a*playerX
-    # b/=(a/0.2)
-    # a/=(a/0.2)
-    diLaserX,diLaserY=a,b
-    print (a,b)
+   
+    laserA,laserB=a,b
     laserX,laserY= eX,eY
 
 def crt():
-    global eX,eY,diX,diY,speedY,laserState
-    eX=random.choice([0,568])
-    eY=random.randint(0,468)
-    if eX==0:
+    global eX,eY,diX,diY
+    # eX=random.choice([0,568])
+    # eY=random.randint(0,468)
+    if eX<=0:
         diX=1
-    if eX==568:
+    if eX>=536:
         diX=-1
-    diY=random.choice([1,-1])
-    speedY=random.uniform(0.1,0.3)
-    laserState=0
-
-# print (random.choice([0,568]))
+    if eY<=0:
+        diY=1
+    if eY>=436:
+        diY=-1
 
 #Game Loop
 running= True
@@ -89,31 +88,27 @@ while running:
             if event.key==pygame.K_DOWN or event.key== pygame.K_UP:
                 changeY=0
     
-    #Laser
-    # if laserState ==0 :
-    #     fire()
-    # if laserX<-25 or laserX>600:
-    #     laserState=0
-    # laserX+= diLaserX*0.2
-    # laserY+= diLaserY*0.02
+     #Enenmy
+    crt()
+    eX += 0.1 *diX
+    eY += 0.1 *diY
 
-    # #Enenmy
-    # if eX<-128 or eX>600:
-    #     crt()
-    # eX += 0.3 *diX
-    # eY += speedY *diY
-    # #Player
-    # if 0<playerX+changeX<568:
-    #     playerX+=changeX
-    # if 0<playerY+changeY<468:
-    #     playerY+=changeY
+    # Laser
+    if laserState == 0 :
+        fire()
+    if laserX<-25 or laserX>600:
+        laserState=0
+    laserX+= 0.25*diLaser
+    laserY= laserA*laserX+ laserB
+    
+    #Player
+    if 0<playerX+changeX<568:
+        playerX+=changeX
+    if 0<playerY+changeY<468:
+        playerY+=changeY
 
     enemy(eX,eY)
     laser(laserX,laserY)
     player(playerX,playerY)
-    crt()
-    print (eX,eY)
-    print (playerX,playerY)
-    fire()
     pygame.display.update()
-    break
+    # break
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-print (eX,eY)
+r
