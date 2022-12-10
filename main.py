45742af600a9504793bf58b8fcd902375bfa25de
Neil-275 <onghoangcodebug01@gmail.com>
2022-12-10 23:45:34 +0700

    Fail

--- main.py
+++ main.py
@@ -4,28 +4,38 @@ import pygame
 
 #intialize pygame
 pygame.init()
+
 #Adjust to resize screen
 maxW,maxH=900,500   
 screen= pygame.display.set_mode((maxW,maxH))
 
 #Background
 background= pygame.image.load("img/starbg.jpg")
-
+pygame.mixer.music.load('soundEffect/background.wav')
+pygame.mixer.music.play(-1)
+pygame.mixer.music.set_volume(0.05)
 
 #Title and background
 pygame.display.set_caption("Hello World")
 icon= pygame.image.load('img/logo.png')
 pygame.display.set_icon(icon)
 gameover= pygame.image.load('img/gameover.png')
+font = pygame.font.Font('freesansbold.ttf',32)
 
 #Animation
 explode = pygame.image.load('img/explode.png')
+explodeSound = pygame.mixer.Sound('soundEffect/explosion.wav')
+explodeSound.set_volume(0.05)
+
+#Score
+textX,textY=5,5
+score=-1
+scoreText=font.render(("Coin:"+ str(score)),True,(255,255,255))
 
 #Coin
 coinImg= pygame.image.load('img/coin.png') # Trying to add a gif instead of png file
 coinX,coinY=0,0
 coinEdge=24
-score=0
 coinState=0
 difficulty=0
 
@@ -36,6 +46,8 @@ laserX,laserY=[],[]
 disX,disY,dis=[],[],[]
 diLaserX,diLaserY=[],[]
 laserEdge=24
+laserSound= pygame.mixer.Sound('soundEffect/laser.wav')
+laserSound.set_volume(0.05)
 
 #Player
 playerImg=pygame.image.load("img/worldwide.png")
@@ -53,6 +65,7 @@ def updateDisplay(State,x,y):
     screen.blit(State,(x,y))
 
 def fire(i):
+    laserSound.play()
     global playerX,playerY,eX,eY,laserX,laserY,laserState,diLaserX,diLaserY,dis,disX,disY
     laserState[i]=1
     diLaserX[i]= abs(playerX-eX[i])/(playerX-eX[i])
@@ -65,8 +78,8 @@ def newCoin():
     global coinX,coinY,score,coinState
     coinState=1
     score+=1
-    coinX=random.randint(0,maxW-coinEdge)
-    coinY=random.randint(0,maxH-coinEdge)
+    coinX=random.randint(6,maxW-coinEdge)
+    coinY=random.randint(6,maxH-coinEdge)
 
 def isCollide(a,b,x,y):
     if sqrt((a-x)*(a-x)+(b-y)*(b-y))< 30:
@@ -102,10 +115,12 @@ def increaseDifficulty():
 
 #Game Loop
 running= True
+
 while running:
     screen.fill((4,4,4))
     screen.blit(background,(0,0))
-        
+    # tmp=pygame.display.get_window_size()
+    # print (tmp)
     #Event
     for event in pygame.event.get():
         if  event.type == pygame.QUIT:
@@ -132,6 +147,7 @@ while running:
     if score>=difficulty*5 and len(eX)<5:
         difficulty+=1
         increaseDifficulty()
+
     #enenmyMovement
     for i in range(len(eX)):
         bounce(i)
@@ -145,33 +161,41 @@ while running:
             fire(i)
         if laserX[i]< -laserEdge or laserX[i]>maxW or laserY[i]< -laserEdge or laserY[i]>maxH:
             laserState[i]=0
-        if laserState[i]==1:
-            laserX[i]+= 0.3*(maxW/600)*disX[i]/dis[i] * diLaserX[i]
-            laserY[i]+= 0.3*(maxW/600)*disY[i]/dis[i] * diLaserY[i]
-        updateDisplay(laserImg,laserX[i],laserY[i])
+        
+        laserX[i]+= 0.3*(maxW/600)*disX[i]/dis[i] * diLaserX[i]
+        laserY[i]+= 0.3*(maxW/600)*disY[i]/dis[i] * diLaserY[i]
+        if laserState[i]!=2:
+            updateDisplay(laserImg,laserX[i],laserY[i])
 
     #Collision
-    if isCollide(playerX,playerY,eX[i],eY[i]) or isCollide(playerX,playerY,laserX[i],laserY[i]) and alive==1:
-        alive=0
-        for i in range(len(eX)):
-            laserState[i]=0
-    if isCollide(playerX,playerY,coinX,coinY) or coinState==0:
+    for i in range (len(eX)):
+        if isCollide(playerX,playerY,eX[i],eY[i]) or isCollide(playerX,playerY,laserX[i],laserY[i]) and alive==1:
+            alive=0
+            laserState[i]=2
+    if isCollide(playerX,playerY,coinX,coinY) or score==-1:
         newCoin()
+        scoreText=font.render(("Coin:"+ str(score)),True,(255,255,255))
         
     updateDisplay(coinImg,coinX,coinY)
 
+    tmp=pygame.time.Clock().tick(0)
+    print(tmp)
     #playerMovement
-    
     if alive==1:
         updateDisplay(playerImg,playerX,playerY)
     else :
         # Lose
+        explodeSound.play()
         updateDisplay(explode,playerX-48,playerY-48)
-        screen.blit(gameover,(270,70))
+        screen.blit(gameover,(270,70))  
+        changeX,changeY=0,0
+        
     if 0<playerX+changeX<maxW-laserEdge:
         playerX+=changeX
     if 0<playerY+changeY<maxH-laserEdge:
         playerY+=changeY
+    #Score
+    updateDisplay(scoreText,textX,textY)
     pygame.display.update()
     # break
 
